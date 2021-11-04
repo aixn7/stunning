01b565b77e5f49794c069fe33b1083d95da19a4c
Ivar Fernandez <ivarfermar7@gmail.com>
2021-11-04 13:19:16 +0100

    001 Cambios en app y readme

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Ivar Fernandez");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("4 de noviembre");
+    System.out.println("León");
   }
   
 }
